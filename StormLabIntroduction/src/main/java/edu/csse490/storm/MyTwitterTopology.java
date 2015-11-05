e0e454ecb0ba6ad3ed486414710ab8d4c76c67be
Collin Trowbridge <trowbrct@rose-hulman.edu>
2015-11-05 00:16:42 -0500

    Added more to the lab and the lab code.

--- StormLabIntroduction/src/main/java/edu/csse490/storm/MyTwitterTopology.java
+++ StormLabIntroduction/src/main/java/edu/csse490/storm/MyTwitterTopology.java
@@ -1,10 +1,16 @@
 package edu.csse490.storm;
 
 import storm.starter.spout.TwitterSampleSpout;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
 
 public class MyTwitterTopology {
-	public static void main(String[] args){
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException{
 		String[] keyWords = {"Tokyo", "New York"};
 		
 		TopologyBuilder builder = new TopologyBuilder();
@@ -12,6 +18,14 @@ public class MyTwitterTopology {
 				new TwitterSampleSpout("CONSUMER KEY", "CONSUMER SECRET",
 						"ACCESS KEY", "ACCESS TOKEN", keyWords));
 		
+		builder.setBolt("writer", new WritingBolt())
+			.shuffleGrouping("twitter");
 		
+		Config config = new Config();
+		config.setDebug(false);
+		config.setNumWorkers(1);
+		
+		StormSubmitter.submitTopology("twitter", config, builder.createTopology());
+		System.out.println("Twitter topology should be running...");
 	}
 }
